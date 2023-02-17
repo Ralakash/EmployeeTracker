23aa25e75fb79ff27fc68621f29bbd3d57b89d65
Ralakash <shawnkaralash@gmail.com>
2023-02-17 15:25:10 -0500

    starts to flesh out functions and adds the database class in the DB folder.

---
+++ db/index.js
@@ -0,0 +1,13 @@
+const connection = require('./connection');
+
+class DB {
+	constructor(connection) {
+		this.connection = connection;
+	}
+
+	findAllDepartments() {
+		return this.connection.promise().query('SELECT department.id, department.name FROM department');
+	}
+}
+
+module.exports = new DB(connection);
--- index.js
+++ index.js
@@ -1,9 +1,80 @@
 const { prompt } = require('inquirer');
 const db = require('./db');
 
-const mainMenu = () => {};
+const mainMenu = async () => {
+	prompt([
+		{
+			type: 'list',
+			name: 'choice',
+			message: 'Please select from the following options',
+			choices: [
+				{
+					name: 'View all departments',
+					value: 'VIEW_DEPARTMENTS',
+				},
+				{
+					name: 'View all roles',
+					value: 'VIEW_ROLES',
+				},
+				{
+					name: 'View all employees',
+					value: 'VIEW_EMPLOYEES',
+				},
+				{
+					name: 'Add a department',
+					value: 'ADD_DEPARTMENT',
+				},
+				{
+					name: 'Add a role',
+					value: 'ADD_ROLE',
+				},
+				{
+					name: 'Add an employee',
+					value: 'ADD_EMPLOYEE',
+				},
+				{
+					name: 'Update an employee role',
+					value: 'UPDATE_ROLE',
+				},
+				{
+					name: 'Quit',
+					value: 'QUIT',
+				},
+			],
+		},
+	]).then((res) => {
+		let choice = res.choice;
+		switch (choice) {
+			case 'VIEW_DEPARTMENTS':
+				viewAllDepartments();
+				break;
+			case 'VIEW_ROLES':
+				viewAllRoles();
+				break;
+			case 'VIEW_EMPLOYEES':
+				viewAllEmployees();
+				break;
+			case 'ADD_DEPARTMENT':
+				addDepartment();
+				break;
+			case 'ADD_ROLE':
+				addRole();
+				break;
+			case 'ADD_EMPLOYEE':
+				addEmployee();
+				break;
+			case 'UPDATE_ROLE':
+				updateRole();
+				break;
+			default:
+				quit();
+		}
+	});
+};
 
-const viewAllDepartments = () => {};
+const viewAllDepartments = () => {
+	db.find;
+};
 
 const viewAllRoles = () => {};
 
@@ -16,3 +87,12 @@ const addRole = () => {};
 const addEmployee = () => {};
 
 const updateRole = () => {};
+
+const quit = () => {
+	console.log('Goodbye!');
+	process.exit();
+};
+
+const init = () => {
+	mainMenu();
+};
