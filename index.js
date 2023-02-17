54c5cbe79584706e0579a1b211bb105339066cc8
Ralakash <shawnkaralash@gmail.com>
2023-02-16 22:16:19 -0500

    adds basic outline for functions needed, fixes employees needing a manager

---
+++ index.js
@@ -0,0 +1,18 @@
+const { prompt } = require('inquirer');
+const db = require('./db');
+
+const mainMenu = () => {};
+
+const viewAllDepartments = () => {};
+
+const viewAllRoles = () => {};
+
+const viewAllEmployees = () => {};
+
+const addDepartment = () => {};
+
+const addRole = () => {};
+
+const addEmployee = () => {};
+
+const updateRole = () => {};
